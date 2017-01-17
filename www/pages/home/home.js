cc980bf9244f1c3b79f479c9bc269ddb29c4ee4a
Brendan Flynn <brendanpflynn@gmail.com>
2017-01-17 15:44:36 -0500

    updates home screen - order of apps, splits into active, controls, and future, removes snake

--- www/pages/home/home.js
+++ www/pages/home/home.js
@@ -4,59 +4,83 @@
     angular.module('flexvolt.home', [])
     .controller('HomeCtrl', ['$scope', function($scope){
         $scope.apps =
-            {  row1:
-
-                {
-                    b1: {
-                        icon:"icon ion-information-circled",
-                        ref:"intro",
-                        btnName:"Get Started"
+            {
+                active: {
+                    row1: {
+                        b1: {
+                            icon:"icon ion-information-circled",
+                            ref:"intro",
+                            btnName:"Get Started"
+                        },
+                        b2: {
+                            icon:"icon ion-ios-pulse-strong",
+                            ref:"trace",
+                            btnName:"Trace"
+                        },
+                        b3: {
+                            icon:"icon ion-ios-navigate",
+                            ref:"godot",
+                            btnName:"Go Dot"
+                        },
+                        b4:{
+                            icon:"icon ion-ios-pulse",
+                            ref:"rms",
+                            btnName:"RMS Plot"
+                        }
                     },
-                    b2: {
-                        icon:"icon ion-ios-pulse-strong",
-                        ref:"trace",
-                        btnName:"Trace"
-                    },
-                    b3: {
-                        icon:"icon ion-ios-navigate",
-                        ref:"godot",
-                        btnName:"Go Dot"
-                    },
-                    b4: {
-                        icon:"icon ion-ios-game-controller-b",
-                        ref:"snake",
-                        btnName:"Snake Game"
+                    row2: {
+                        b1: {
+                            icon:"icon ion-speedometer",
+                            ref:"myometer",
+                            btnName:"Myometer"
+                        }
                     }
                 },
-                row2:
-                {
-                    b1:{
-                        icon:"icon ion-speedometer",
-                        ref:"myometer",
-                        btnName:"Myometer"
-                    },
-                    b2:{
-                        icon:"icon ion-ios-heart",
-                        ref:"ekg",
-                        btnName:"EKG"
-
-                    },
-                    b3:{
-                        icon:"icon ion-android-stopwatch",
-                        ref:"hrv",
-                        btnName:"HRV"
-                    },
-                    b4:{
-                        icon:"icon ion-ios-pulse",
-                        ref:"rms",
-                        btnName:"RMS Plot"
+                controls: {
+                    row1: {
+                        b1: {
+                            icon:"icon ion-briefcase",
+                            ref:"demos",
+                            btnName:"Demos"
+                        },
+                        b2: {
+                            icon:"icon ion-help",
+                            ref:"help",
+                            btnName:"Help"
+                        },
+                        b3: {
+                            icon:"icon ion-settings",
+                            ref:"settings",
+                            btnName:"Settings"
+                        },
+                        b4: {
+                            icon:"icon ion-radio-waves",
+                            ref:"connection",
+                            btnName:"Connection"
+                        }
                     }
-    //                    b4:{
-    //                        icon:"icon ion-leaf",
-    //                        ref:"home",
-    //                        btnName:"Relax"
-    //                    }
                 },
+                development: {
+                    row1: {
+                        b2:{
+                            icon:"icon ion-ios-heart",
+                            ref:"ekg",
+                            btnName:"EKG"
+
+                        },
+                        b3:{
+                            icon:"icon ion-android-stopwatch",
+                            ref:"hrv",
+                            btnName:"HRV"
+                        }
+                        // b4: {
+                        //     icon:"icon ion-ios-game-controller-b",
+                        //     ref:"snake",
+                        //     btnName:"Snake Game"
+                        // }
+                    }
+                }
+            }
     //                row3:{
     //                    b1:{
     //                        icon:"icon ion-nuclear",
@@ -79,34 +103,18 @@
     //                        btnName:"controller"
     //                    }
     //                },
-                row4:{
+                // row4:{
     //                    b1:{
     //                        icon:"icon ion-navigate",
     //                        ref:"circle",
     //                        btnName:"test"
     //                    },
-                    b1:{
-                        icon:"icon ion-briefcase",
-                        ref:"demos",
-                        btnName:"Demos"
-                    },
-                    b2:{
-                        icon:"icon ion-help",
-                        ref:"help",
-                        btnName:"Help"
-                    },
-                    b3:{
-                        icon:"icon ion-gear-b",
-                        ref:"settings",
-                        btnName:"Settings"
-                    },
-                    b4:{
-                        icon:"icon ion-settings",
-                        ref:"connection",
-                        btnName:"Connection"
-                    }
-                }
-            };
+
+                  //                    b4:{
+                  //                        icon:"icon ion-leaf",
+                  //                        ref:"home",
+                  //                        btnName:"Relax"
+                  //                    }
     }])
 
 }())
