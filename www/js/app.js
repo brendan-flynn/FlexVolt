8e5d4dba94c8290583b1718c660ec05bb093e5d9
Brendan Flynn <brendanpflynn@gmail.com>
2018-03-02 17:07:11 -0500

    android fix - issues with keyboard plugin

--- www/js/app.js
+++ www/js/app.js
@@ -44,9 +44,9 @@
       $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
-        if(window.cordova && window.cordova.plugins.Keyboard) {
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
+        // if(window.cordova && window.cordova.plugins.Keyboard) {
+        //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        // }
         if(window.StatusBar) {
           // org.apache.cordova.statusbar required
           StatusBar.styleDefault();
