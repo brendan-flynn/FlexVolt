17026098822e1f2cc4864905ddef00fcdf1a4401
Brendan Flynn <brendanpflynn@gmail.com>
2018-06-14 01:21:34 -0400

    Improvement - updates hardware settings page to new style, adds modals for settings with more than 2 choices (channels, rms window size)

--- www/pages/settings/hardware/hardware.js
+++ www/pages/settings/hardware/hardware.js
@@ -9,8 +9,8 @@
         };
     })
     .controller('HardwareCtrl',
-    ['$scope','$state','flexvolt','hardwareLogic','file','appLogic',
-    function($scope, $state, flexvolt, hardwareLogic, file, appLogic) {
+    ['$scope','$state','$ionicModal','flexvolt','hardwareLogic','file','appLogic',
+    function($scope, $state, $ionicModal, flexvolt, hardwareLogic, file, appLogic) {
         var currentUrl = $state.current.url;
         console.log('currentUrl = '+currentUrl);
 
@@ -28,13 +28,64 @@
         $scope.onChange = function(){
             flexvolt.api.validateSettings();
             console.log('settings now: '+angular.toJson(hardwareLogic.settings));
-            $scope.rmsWindowSize = Math.pow(2, hardwareLogic.settings.rmsWindowSizePower);
             hardwareLogic.updateSettings();
             flexvolt.api.updateSettings();
         };
 
         $scope.app = {
           version: appLogic.dm.version
+        };
+
+        $ionicModal.fromTemplateUrl('pages/settings/hardware/rmswindowsize.html', {
+            scope: $scope
+        }).then(function(modal){
+            $scope.rmsWindowSizeModal = modal;
+        });
+
+        $scope.showRmsWindowSizeModal = function(){
+            $scope.rmsWindowSizeModal.show();
+        };
+
+        $scope.selectedRMSWindowSizeStyle = function(index) {
+          if (hardwareLogic.rmsWindowList[index].value === hardwareLogic.settings.rmsWindowSizePower) {
+            return "active";
+          }
+        };
+
+        $scope.selectRMSWindowSize = function(index) {
+            console.log('selected scale: ' + hardwareLogic.rmsWindowList[index] + ', via index: ' + index);
+            hardwareLogic.settings.rmsWindowSizePower = hardwareLogic.rmsWindowList[index].value;
+            hardwareLogic.settings.smoothFilterVal = hardwareLogic.settings.rmsWindowSizePower;
+            $scope.rmsWindowSize = Math.pow(2, hardwareLogic.settings.rmsWindowSizePower);
+            $scope.onChange();
+        };
+
+
+        $ionicModal.fromTemplateUrl('pages/settings/hardware/channels.html', {
+            scope: $scope
+        }).then(function(modal){
+            $scope.channelModal = modal;
+        });
+
+        $scope.showChannelModal = function(){
+            $scope.channelModal.show();
+        };
+
+        $scope.selectedChannelStyle = function(index) {
+          if (hardwareLogic.channelList[index].value === hardwareLogic.settings.nChannels) {
+            return "active";
+          }
+        };
+
+        $scope.selectChannel = function(index) {
+            console.log('selected scale: ' + hardwareLogic.channelList[index] + ', via index: ' + index);
+            hardwareLogic.settings.nChannels = hardwareLogic.channelList[index].value;
+            $scope.onChange();
+        };
+
+        $scope.toggleBitDepth10 = function(bitDepth10){
+          hardwareLogic.settings.bitDepth10=bitDepth10;
+          $scope.onChange();
         };
 
     }]);
